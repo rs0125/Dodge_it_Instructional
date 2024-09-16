2a765e7879afd937201b700e774c00347e94ffaf
Raghav Sharma <raghaaav5@gmail.com>
2024-09-16 20:33:07 +0530

    button reimplemented

--- Assets/Basegame/Scripts/movement.cs
+++ Assets/Basegame/Scripts/movement.cs
@@ -6,7 +6,6 @@ using UnityEngine.UIElements;
 
 public class movement : MonoBehaviour
 {
-
     public Rigidbody rb;
     public float ForwardForce = 2000f;
     public float KeySideForce = 500f;
@@ -18,10 +17,25 @@ public class movement : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {   
+    {
         //adds a forward force
         rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
 
+
+
+        // keyboard input (non touch screen)
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(KeySideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-KeySideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+
+        // button input (touchscreen)
         if (right.isPressed)
         {
             rb.AddForce(ButtonSideForce * Time.deltaTime, 0, 0);
@@ -34,21 +48,11 @@ public class movement : MonoBehaviour
         }
 
 
-
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(KeySideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-KeySideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-
+        // if the object is below the position of y axis = -1, end the game
         if (rb.position.y < -1f)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
     }
+
 }
